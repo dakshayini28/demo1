b5478a4ca40ecf6bae3885fb3a598cbdf053165a
dakshayini28 <21bd1a6702csd@gmail.com>
2025-06-27 15:20:44 +0530

    Revert "request body change from param"
    This reverts commit 2e42147f22e8232b105e4230b5fb8a3cce736a1a.

--- src/main/java/com/example/demo1/controller/ConnectionController.java
+++ src/main/java/com/example/demo1/controller/ConnectionController.java
@@ -1,6 +1,5 @@
 package com.example.demo1.controller;
 
-import com.example.demo1.entity.ConnectionDetails;
 import com.example.demo1.entity.DbDetails;
 import com.example.demo1.service.ConnectionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +19,10 @@ public class ConnectionController {
         return connectionService.getConnections();
     }
     @PostMapping("/addConnection")
-    public ResponseEntity<String> addConnection(@RequestBody ConnectionDetails con){
+    public ResponseEntity<String> addConnection(@RequestParam String name,
+                                                @RequestParam String url,@RequestParam String username,@RequestParam String password){
         try {
-            connectionService.addConnection(con.getName(), con.getUrl(), con.getUsername(), con.getPassword());
+            connectionService.addConnection(name, url, username, password);
             return ResponseEntity.status(HttpStatus.CREATED).body("Connection created");
         } catch (RuntimeException e) {
             if (e.getMessage().contains("already exists")) {
@@ -38,16 +38,6 @@ public class ConnectionController {
             return ResponseEntity.ok("deleted");
         }catch(Exception e){
             if(e.getMessage().contains("Connection with given ID does not exist.")) return ResponseEntity.ok("Id doesnt exist");
-            return ResponseEntity.ok("Some error occured");
-        }
-    }
-    @PutMapping("/updateConnection")
-    public ResponseEntity<String> updateConnection(@RequestParam int id,@RequestBody ConnectionDetails con){
-        try{
-            connectionService.updateConnection(id,con.getName(),con.getUrl(),con.getUsername(),con.getPassword());
-            return ResponseEntity.ok("updated");
-        }catch (Exception e){
-            if(e.getMessage().contains("Connection Id not found")) return ResponseEntity.ok("Id doesnt exist");
             return ResponseEntity.ok("Some error occured");
         }
     }
